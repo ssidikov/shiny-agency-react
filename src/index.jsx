d02128cba1bb4b40684d6563bd3ad0bef98d469d
ssidikov <sardorbek.sidikov7@gmail.com>
2024-10-29 15:29:38 +0100

    Refactor code

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,32 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
-import Header from './components/Header'
-import Error from './components/Error'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-    * {
-      font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-
-    body {
-      margin: 0;
-    }
-`
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Error from './components/Error'
+import GlobalStyle from './utils/style/GlobalStyle'
+import { ThemeProvider } from './utils/context'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router basename="/shiny-agency-react">
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/freelances" element={<Freelances />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/survey/:questionNumber" element={<Survey />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/freelances" element={<Freelances />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 )
--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react'
+import { SurveyContext } from '../../utils/context'
+
 function Results() {
+  const { answers } = useContext(SurveyContext)
+  console.log(answers)
+
   return (
     <div>
-      <h1>Your Results</h1>
+      <h1>Résultats</h1>
     </div>
   )
 }

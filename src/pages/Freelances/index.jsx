61c8ae960f69724e43ef4a7b658c41802bd194b9
ssidikov <sardorbek.sidikov7@gmail.com>
2024-11-01 18:45:34 +0100

    Tests for Freelances and Result

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,32 +1,8 @@
-// import DefaultPicture from '../../assets/profile.png'
 import Card from '../../components/Card'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
-import { useEffect, useState } from 'react'
 import { Loader } from '../../utils/style/Atoms'
-
-// const freelancersList = [
-//   {
-//     name: 'John Doe',
-//     title: 'UX Designer',
-//     picture: DefaultPicture,
-//   },
-//   {
-//     name: 'Jane Doe',
-//     title: 'Développeur frontend',
-//     picture: DefaultPicture,
-//   },
-//   {
-//     name: 'John Smith',
-//     title: 'Devops',
-//     picture: DefaultPicture,
-//   },
-//   {
-//     name: 'Jane Smith',
-//     title: 'UX Designer',
-//     picture: DefaultPicture,
-//   },
-// ]
+import { useFetch, useTheme } from '../../utils/hooks'
 
 const CardsContainer = styled.div`
   display: flex;
@@ -40,10 +16,10 @@ const CardsContainer = styled.div`
 `
 
 const PageTitle = styled.h1`
+  font-size: 30px;
   text-align: center;
-  margin-bottom: 30px;
-  color: black;
-  font-size: 2rem;
+  padding-bottom: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const PageSubtitle = styled.h2`
@@ -52,6 +28,7 @@ const PageSubtitle = styled.h2`
   font-weight: 300;
   text-align: center;
   padding-bottom: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const LoaderWrapper = styled.div`
@@ -60,50 +37,35 @@ const LoaderWrapper = styled.div`
 `
 
 function Freelances() {
-  const [isDataLoading, setDataLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [freelancersList, setFreelancersList] = useState([])
+  const { theme } = useTheme()
+  const { data, isLoading, error } = useFetch(
+    `http://localhost:8000/freelances`
+  )
 
-  useEffect(() => {
-    async function fetchFreelances() {
-      setDataLoading(true)
-      try {
-        const response = await fetch('http://localhost:8000/freelances')
-        const { freelancersList } = await response.json()
-        setFreelancersList(freelancersList)
-      } catch (err) {
-        console.log('===== error =====', err)
-        setError(true)
-      } finally {
-        setDataLoading(false)
-      }
-    }
-    fetchFreelances()
-  }, [])
+  const freelancersList = data?.freelancersList
 
   if (error) {
-    return <span>Oups il y a eu un problème</span>
+    return <span>Il y a un problème</span>
   }
 
   return (
     <div>
-      <PageTitle>Trouvez votre prestataire</PageTitle>
-      <PageSubtitle>
+      <PageTitle theme={theme}>Trouvez votre prestataire</PageTitle>
+      <PageSubtitle theme={theme}>
         Chez Shiny nous réunissons les meilleurs profils pour vous.
       </PageSubtitle>
-      {isDataLoading ? (
+      {isLoading ? (
         <LoaderWrapper>
-          <Loader />
+          <Loader theme={theme} data-testid="loader" />
         </LoaderWrapper>
       ) : (
         <CardsContainer>
-          {freelancersList.map((profile, index) => (
+          {freelancersList?.map((profile, index) => (
             <Card
               key={`${profile.name}-${index}`}
               label={profile.job}
               title={profile.name}
               picture={profile.picture}
-              // description={profile.description}
             />
           ))}
         </CardsContainer>
--- src/pages/Freelances/index.test.js
+++ src/pages/Freelances/index.test.js
@@ -1,20 +1,9 @@
 import { rest } from 'msw'
+import '@testing-library/jest-dom/extend-expect'
 import { setupServer } from 'msw/node'
-import { render, waitFor, screen } from '@testing-library/react'
-
-import Freelances from './index'
-
-const server = setupServer(
-  rest.get('http://localhost:8000/freelances', (req, res, ctx) => {
-    return res(ctx.json({ freelancersList: freelancersMockedData }))
-  })
-)
-
-beforeAll(() => server.listen())
-
-afterEach(() => server.resetHandlers())
-
-afterAll(() => server.close())
+import { waitForElementToBeRemoved, screen } from '@testing-library/react'
+import { render } from '../../utils/test'
+import Freelances from './'
 
 const freelancersMockedData = [
   {
@@ -28,3 +17,22 @@ const freelancersMockedData = [
     picture: '',
   },
 ]
+
+const server = setupServer(
+  rest.get('http://localhost:8000/freelances', (req, res, ctx) => {
+    return res(ctx.json({ freelancersList: freelancersMockedData }))
+  })
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+it('Should display freelancers names after loader is removed', async () => {
+  render(<Freelances />)
+
+  await waitForElementToBeRemoved(() => screen.getByTestId('loader'))
+  expect(screen.getByText('Harry Potter')).toBeInTheDocument()
+  expect(screen.getByText('Hermione Granger')).toBeInTheDocument()
+  expect(screen.queryByTestId('loader')).not.toBeInTheDocument()
+})
